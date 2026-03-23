5f37d9e465ad64992111cf132de194c80ed971a4
Ran <ran.yan.v@outlook.com>
2026-03-24 00:11:49 +0100

    process

--- src/swap.c
+++ src/swap.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   operation.c                                        :+:      :+:    :+:   */
+/*   swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rayan <rayan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -14,16 +14,55 @@
 
 void	swap(t_node **stack)
 {
-	t_node	*tmp;
-	t_node	*new;
+	t_node	*first;
+	t_node	*second;
 
-	if (!stack && !(*stack) && !(*stack) -> next)
+	if (!stack || !(*stack) || !(*stack) -> next)
 		return;
 
-	tmp = *stack;
-	new = tmp -> next;
-	tmp -> next = new -> next;
-	if 
-	
-	
+	first = *stack;
+	second = first -> next;
+	first -> next = second -> next;
+	if (second -> next)
+		second -> next -> previous = first;
+	second -> next = first;
+	second -> previous = NULL;
+	first -> previous = second;
+	*stack = second;
+}
+
+void	sa(t_node **a)
+{
+	if (!a || !*a || !(*a) -> next)
+		return ;
+	swap(a);
+	write(1, "sa\n", 3);
+}
+
+void	sb(t_node **b)
+{
+	if (!b || !*b || !(*b) -> next)
+		return ;
+	swap(b);
+	write(1, "sa\n", 3);
+}
+
+//不需要两个同时满足才执行
+void	ss(t_node **a, t_node **b)
+{
+	int	swap;
+
+	swap = 0;
+	if (a && *a && (*a) -> next)
+	{
+		swap(a);
+		swap = 1;
+	}
+	if (b && *b && (*b) -> next)
+	{
+		swap(b);
+		swap = 1;
+	}
+	if (swap == 1)
+		write(1, "ss\n", 3);
 }
